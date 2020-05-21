86cca7651b9d3fbc07ff439f731cf4089513b822
leosatan-fairy <tikhonovich0220@gmail.com>
2020-05-22 02:17:00 +1000

    feat: heart autoplay problem fixed.

--- src/app/shared/doctor-as-section/doctor-as-section.component.ts
+++ src/app/shared/doctor-as-section/doctor-as-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-doctor-as-section',
@@ -9,8 +10,32 @@ export class DoctorAsSectionComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  normalSound = new Howl({
+    src: ['assets/sounds/normal.mp3']
+  });
 
+  aspatientSound = new Howl({
+    src: ['assets/sounds/aspatient.mp3']
+  });
+
+  normalSoundButtonClickCount: boolean;
+  aspatientSoundButtonClickCount: boolean;
+
+  // play normal sound
+  normalSoundPlay() {
+    this.aspatientSound.pause();
+    this.aspatientSound.currentTime = 0;
+    this.normalSound.play();
+  }
+
+  aspatientSoundPlay() {
+    this.normalSound.pause();
+    this.normalSound.currentTime = 0;
+    this.aspatientSound.play();
+  }
+
+  ngOnInit(): void {
+    Howler.volume(1);
   }
 
 }
